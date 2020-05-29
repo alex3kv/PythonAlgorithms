006824fb863b640d638fd3aae246d8f0896d8130
Александр Игнат <alex3kv@gmail.com>
2020-05-29 21:51:22 +0300

    Урок 1 задания 6-7

--- Lesson1/Task8.py
+++ Lesson1/Task8.py
@@ -1,3 +1,17 @@
 # Вводятся три разных числа.  Найти, какое из них является средним (больше
 # одного, но меньше другого).
-pass
+#
+print("Введите три числа: ")
+x1 = int(input())
+x2 = int(input())
+x3 = int(input())
+
+result = x3 
+
+if x2 < x1 < x3 or x3 < x1 < x2:
+    result = x1
+elif x1 < x2 < x3 or x3 < x2 < x1:
+    result = x2
+
+print()
+print(f"Среднее число = {result}")
