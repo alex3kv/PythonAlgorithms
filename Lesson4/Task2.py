39f19804f54b3fc4aec51103ea596cdab56fc8d1
Игнат Александр <ignataf@mosreg.ru>
2020-06-09 18:49:09 +0300

    Урок 4 задание 2 не полное

--- Lesson4/Task2.py
+++ Lesson4/Task2.py
@@ -16,4 +16,55 @@ sieve(5)
 prime(1)
 2
 '''
-pass
+import math
+
+def build_sieve(n):
+	arr = [i for i in range(n)]
+	arr[1] = 0
+
+	for i in range(2, n):
+
+		if arr[i] != 0:			
+			j = i * 2
+
+			while j < n:
+				arr[j] = 0
+				j += i
+
+	return [i for i in arr if i != 0]
+
+print(build_sieve(100))
+
+def sieve(n):
+	sieve = build_sieve(100)	
+	return sieve[n-1]
+
+print(sieve(5))
+
+# проверка числа на простоту
+def is_prime(n):
+
+	if n == 2:
+		return True
+
+	max = math.ceil(math.sqrt(n))
+	for i in range(2, max + 1):
+		if n % i == 0:
+			return False
+	return True
+
+# находит n-ое простое число
+def prime(max_index):
+	i = 0
+	number = 1
+	while True:		
+		number += 1
+
+		if is_prime(number):
+			i += 1
+		
+		if i == max_index:
+			break
+	return number
+
+print(prime(5))
