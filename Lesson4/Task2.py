a0fc2b8ab5b027d56f83206510301f7605f7b37c
Игнат Александр <ignataf@mosreg.ru>
2020-06-10 14:29:55 +0300

    Урок 4 задание 2

--- Lesson4/Task2.py
+++ Lesson4/Task2.py
@@ -17,6 +17,9 @@ prime(1)
 2
 '''
 import math
+import cProfile
+
+N = 1000
 
 def build_sieve(n):
 	arr = [i for i in range(n)]
@@ -33,13 +36,21 @@ def build_sieve(n):
 
 	return [i for i in arr if i != 0]
 
-print(build_sieve(100))
-
 def sieve(n):
-	sieve = build_sieve(100)	
-	return sieve[n-1]
+	k = 1
 
-print(sieve(5))
+	if  n < 100:
+		k = 5
+	if 100 < n and n <= 1000:
+		k = 8
+	if 1000 < n and n <= 2000:
+		k = 9
+	if 2000 < n and n <= 3000:
+		k = 10
+
+	sieve = build_sieve(k * n)	
+	return sieve[n - 1]
+
 
 # проверка числа на простоту
 def is_prime(n):
@@ -67,4 +78,16 @@ def prime(max_index):
 			break
 	return number
 
-print(prime(5))
+print(sieve(N))
+print(prime(N))
+
+cProfile.run(f"sieve({N})")
+cProfile.run(f"prime({N})")
+
+'''
+          500    1000    2000    3500             
+sieve   0.003   0.006   0.012   0.022   
+prime   0.013   0.027   0.064   0.110
+
+Наиболее оптимальный алгоритм для нахождения простых чисел «Решето Эратосфена» если применить формулу распределения простых чисел в натуральном рядоу можно добиться наиболее оптимальных результатов
+'''
