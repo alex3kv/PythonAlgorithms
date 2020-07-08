39597a674c394742178f4ec08f300b88c97d0aeb
Игнат Александр <ignataf@mosreg.ru>
2020-07-08 18:16:58 +0300

    Урок 8 задача 2 и 3 наброски

--- Lesson8/Task2.py
+++ Lesson8/Task2.py
@@ -1,4 +1,44 @@
 '''
 Доработать алгоритм Дейкстры (рассматривался на уроке), чтобы он дополнительно возвращал список вершин, которые необходимо обойти.
 '''
-pass
+
+def dijkstra(graph, start):
+    lenght = len(graph)
+    is_visited = [False] * lenght
+    cost = [float("inf")] * lenght
+    parent = [-1] * lenght
+
+    cost[start] = 0
+    min_cost = 0
+
+    while min_cost < float("inf"):
+
+        is_visited[start] = True
+
+        for i, vertex in enumerate(graph[start]):
+            if vertex != 0 and not is_visited[i]:
+                if cost[i] > vertex + cost[start]:
+                    cost[i] = vertex + cost[start]
+                    parent[i] = start
+
+        min_cost = float("inf")
+        for i in range(lenght):
+            if min_cost > cost[i] and not is_visited[i]:
+                min_cost = cost[i]
+                start = i
+
+    return cost
+
+#        0 1 2 3 4 5 6 7
+graf = [[0, 0, 1, 1, 9, 0, 0, 0],
+        [0, 0, 9, 4, 0, 0, 7, 0],
+        [0, 9, 0, 0, 3, 0, 6, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 5, 0],
+        [0, 0, 7, 0, 8, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 2, 0]]
+
+result = dijkstra(graf, 0)
+
+print(result)
--- Lesson8/Task3.py
+++ Lesson8/Task3.py
@@ -5,4 +5,43 @@
 a. граф должен храниться в виде списка смежности;
 b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин.
 '''
-pass
+
+def bfs(graph, start, finish):
+    parent = [None for _ in range(len(graph))]
+    is_visited = [False for _ in range(len(graph))]
+
+    deq = deque([start])
+    is_visited[start] = True
+
+    while len(deq) > 0:
+
+        curent = deq.pop()
+
+        if curent == finish:
+            return parent
+            break
+
+        for i, vertex in enumerate(graph[curent]):
+            if vertex == 1 and not is_visited[1]:
+                is_visited[i] = True
+                parent[i] = curent
+                deq.appendleft(i)
+
+    else:
+        return f"Из вершины {start} нельзя попасть в вершину {finish}"
+
+    cost = 0
+    way = deque([finish])
+    i = finish
+
+    while parent[i] != start:
+        cost += 1
+        way.appendleft(parent[i])
+        i = parent[i]
+
+    cost += 1
+    way.appendleft(start)
+
+    return f"кратчайший путь {list(way)} длинной {cost} условных единиц"
+
+
